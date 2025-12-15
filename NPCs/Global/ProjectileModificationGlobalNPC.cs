a327e50020ff41d6464c28d6235673d4d913a522
Megaslayer5656 <justinbex08@gmail.com>
2025-12-15 00:11:43 -0500

    Made the salewa do stuff and added some projectile things (that I stole from examplemod)

---
+++ NPCs/Global/ProjectileModificationGlobalNPC.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria.ModLoader;
+
+namespace MogMod.NPCs.Global
+{
+    public class ProjectileModificationGlobalNPC : GlobalNPC
+    {
+        public override bool InstancePerEntity => true;
+        public int timesHitByModifiedProjectiles;
+    }
+}
