bbab8ae36c96f3cfd9673441d7cbb2e8544f396d
Megaslayer5656 <justinbex08@gmail.com>
2025-12-15 00:36:54 -0500

    Got armlet toggle working w/ same keybind
    I LOVE timers
    
    Will make it do things I'm going to sleep
    
    Willy Kutter

--- Common/Player/MogPlayer.cs
+++ Common/Player/MogPlayer.cs
@@ -20,6 +20,8 @@ namespace MogMod.Common.Player
         // add this for every buff you apply
         public bool isWearingGlimmerCape = false;
         public bool armletActive = false;
+        public int armletTimer = 0;
+        public int armletTimerMax = 120;
         public enum MewingType
         {
             mewingguide = 0
@@ -29,24 +31,36 @@ namespace MogMod.Common.Player
         public override void ProcessTriggers(TriggersSet triggersSet)
         {
             // give debuff cd , 600 = 10 seconds
-                int debuff1 = ModContent.BuffType<Buffs.GlimmerCapeDebuff>();
+            int debuff1 = ModContent.BuffType<Buffs.GlimmerCapeDebuff>();
+
             if (KeybindSystem.GlimmerCapeKeybind.JustPressed && isWearingGlimmerCape && !Player.HasBuff(debuff1))
-                {
+            {
                 int buff1 = ModContent.BuffType<Buffs.GlimmerCapeBuff>();
                 Player.AddBuff(buff1, 1800);
                 Player.AddBuff(debuff1, 3600);
                 // Main.NewText("applied glimmer cape"); //RandomBuffText.Format(Lang.GetBuffName(buff)));
             }
+
             int buff2 = ModContent.BuffType<Buffs.ArmletOfMordiggianBuff>();
+
             if (KeybindSystem.ArmletKeybind.JustPressed && armletActive) //&& !Player.HasBuff(buff2))
-                {
-                Player.AddBuff(buff2, 9999999);
-              }
-            if (KeybindSystem.ArmletKeybind.JustPressed && armletActive && Player.HasBuff(buff2))
             {
-                Player.ClearBuff(buff2);
+                if (armletTimer <= armletTimerMax)
+                {
+                    Player.AddBuff(buff2, 9999999);
+                    armletTimer += 1;
+                } else if (armletTimer >= armletTimerMax)
+                {
+                    Player.ClearBuff(buff2);
+                    armletTimer = 0;
+                }
             }
+
+            while (armletTimer >= 1 && armletTimer <= armletTimerMax + 1)
+            {
+                armletTimer += 1;
             }
+        }
 
         public override void ResetEffects()
         {
