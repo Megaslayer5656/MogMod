6a999c0c7395f4d5d733af6d5a3b4e3675cc78c8
ghostred08 <williamkutter@gmail.com>
2026-01-14 00:46:46 -0500

    boulder bullet behaves like a boulder
    w kez quotes:
    All this and good looks too.
    My sword, my beak, and myself! Three sharpest things in this forest.
    Let's go bust up an Ancient.
    Beak: Yep. Swords: Yep. Wings: Don't work. Let's go.
    Alright, time to unfurl the wings, the ol' flappers, the big guns, he he he... they don't do anything.
    Team looks good, forest looks right. This one's ours, guys.
    Birbin'.
    Just over there? You're the boss.
    I'm unflappable.
    Let's get the flock outta here.
    Followin' the bread crumbs.
    Another day, another... what are we chasing again?
    Let me tell you what your problem is. Me.
    Bird is the word.
    The word is bird.
    One for the good guys.
    When you get to hell, tell 'em a bird sent you.
    Bird-tality!
    Bow to the bird king? Hm hm hm hm hm.
    I'm gonna come back, and I'm gonna kill you. Yeah, you. I'm talking to you, tough guy.
    What the flock?

--- Projectiles/RangedProjectiles/BoulderBulletProj.cs
+++ Projectiles/RangedProjectiles/BoulderBulletProj.cs
@@ -20,36 +20,122 @@ namespace MogMod.Projectiles.RangedProjectiles
             Projectile.width = Projectile.height = 32;
             Projectile.aiStyle = ProjAIStyleID.Arrow;
             Projectile.friendly = true;
-            Projectile.hostile = false;
             Projectile.DamageType = DamageClass.Ranged;
-            Projectile.penetrate = 1;
+            Projectile.penetrate = -1;
             Projectile.timeLeft = 600;
             Projectile.ignoreWater = false;
-            Projectile.tileCollide = false;
-            Projectile.extraUpdates = 1;
-
-            AIType = ProjectileID.Bullet;
+            Projectile.tileCollide = true;
+            Projectile.aiStyle = Terraria.ID.ProjAIStyleID.Boulder;
         }
         public override void AI()
         {
-            //Code to make it not shoot backwards
-            Projectile.spriteDirection = Projectile.direction = (Projectile.velocity.X > 0).ToDirectionInt();
-            Projectile.rotation = Projectile.velocity.ToRotation() + (Projectile.spriteDirection == 1 ? 0f : MathHelper.Pi);
+            // vanilla terraria boulder code
 
-            //Gravity
+            // something in here makes the boulder turn on wall hit (which we dont want to happen)
+            if (this.Projectile.ai[0] != 0f && Projectile.velocity.Y <= 0f && Projectile.velocity.X == 0f)
+            {
+                float num218 = 0.5f;
+                int i2 = (int)((base.Projectile.position.X - 8f) / 16f);
+                int num219 = (int)(base.Projectile.position.Y / 16f);
+                bool flag9 = false;
+                bool flag10 = false;
+                if (WorldGen.SolidTile(i2, num219) || WorldGen.SolidTile(i2, num219 + 1))
+                {
+                    flag9 = true;
+                }
+                i2 = (int)((base.Projectile.position.X + (float)Projectile.width + 8f) / 16f);
+                if (WorldGen.SolidTile(i2, num219) || WorldGen.SolidTile(i2, num219 + 1))
+                {
+                    flag10 = true;
+                }
+                if (flag9)
+                {
+                    Projectile.velocity.X = num218;
+                }
+                else if (flag10)
+                {
+                    Projectile.velocity.X = 0f - num218;
+                }
+                else
+                {
+                    i2 = (int)((base.Projectile.position.X - 8f - 16f) / 16f);
+                    num219 = (int)(base.Projectile.position.Y / 16f);
+                    flag9 = false;
+                    flag10 = false;
+                    if (WorldGen.SolidTile(i2, num219) || WorldGen.SolidTile(i2, num219 + 1))
+                    {
+                        flag9 = true;
+                    }
+                    i2 = (int)((base.Projectile.position.X + (float)Projectile.width + 8f + 16f) / 16f);
+                    if (WorldGen.SolidTile(i2, num219) || WorldGen.SolidTile(i2, num219 + 1))
+                    {
+                        flag10 = true;
+                    }
+                    if (flag9)
+                    {
+                        Projectile.velocity.X = num218;
+                    }
+                    else if (flag10)
+                    {
+                        Projectile.velocity.X = 0f - num218;
+
+                    }
+                    else
+                    {
+                        i2 = (int)((base.Projectile.position.X - 8f - 32f) / 16f);
+                        num219 = (int)(base.Projectile.position.Y / 16f);
+                        flag9 = false;
+                        flag10 = false;
+                        if (WorldGen.SolidTile(i2, num219) || WorldGen.SolidTile(i2, num219 + 1))
+                        {
+                            flag9 = true;
+                        }
+                        i2 = (int)((base.Projectile.position.X + (float)Projectile.width + 8f + 32f) / 16f);
+                        if (WorldGen.SolidTile(i2, num219) || WorldGen.SolidTile(i2, num219 + 1))
+                        {
+                            flag10 = true;
+                        }
+                        if (!flag9 && !flag10)
+                        {
+                            if ((int)(base.Projectile.Center.X / 16f) % 2 == 0)
+                            {
+                                flag9 = true;
+                            }
+                            else
+                            {
+                                flag10 = true;
+                            }
+                        }
+                        if (flag9)
+                        {
+                            Projectile.velocity.X = num218;
+                        }
+                        else if (flag10)
+                        {
+                            Projectile.velocity.X = 0f - num218;
+                        }
+                    }
+                }
+            }
+
+            // rotate boulder
+            Projectile.rotation += Projectile.velocity.X * 0.06f;
+            this.Projectile.ai[0] = 1f;
+            
+            // makes it heavy
             Projectile.velocity.Y = Projectile.velocity.Y + 0.20f;
             if (Projectile.velocity.Y > 16f)
             {
                 Projectile.velocity.Y = 16f;
             }
-            if (Projectile.timeLeft < 595)
-                Projectile.tileCollide = true;
 
-            if (Projectile.timeLeft < 570)
+            // hurt player after some time
+            if (Projectile.timeLeft < 580)
             {
                 Projectile.hostile = true;
             }
 
+            // dust effect
             for (int k = 0; k < 1; k++)
             {
                 int d = Dust.NewDust(Projectile.position, Projectile.width, Projectile.height, DustID.Stone, Projectile.velocity.X * 0.25f, Projectile.velocity.Y * 0.25f, 150, default, 0.3f);
@@ -57,6 +143,7 @@ namespace MogMod.Projectiles.RangedProjectiles
                 Main.dust[d].noLight = true;
             }
         }
+        public override bool CanHitPvp(Player target) => false;
         public override void OnHitNPC(NPC target, NPC.HitInfo hit, int damageDone)
         {
             target.AddBuff(BuffID.Dazed, 180);
@@ -67,10 +154,40 @@ namespace MogMod.Projectiles.RangedProjectiles
         }
         public override bool OnTileCollide(Vector2 oldVelocity)
         {
-            Projectile.Kill();
+            // this makes the projectile bounce once
+
+            if (Projectile.ai[0] == 1)
+            {
+                // how many times it can bounce
+
+                // to make it bounce more than once use this code below
+
+                //if (Projectile.ai[1] >= 1f)
+                //{
+                //    Projectile.Kill();
+                //}
+                //else
+                //{
+                    if (Projectile.ai[1] < 1f)
+                    {
+                        Collision.HitTiles(Projectile.position + Projectile.velocity, Projectile.velocity, Projectile.width, Projectile.height);
+                        SoundEngine.PlaySound(SoundID.Dig, Projectile.position);
+                        if (Projectile.velocity.X != oldVelocity.X)
+                        {
+                            Projectile.velocity.X = -oldVelocity.X;
+                        }
+
+                        // bounce height
+                        if (Projectile.velocity.Y != oldVelocity.Y)
+                        {
+                            Projectile.velocity.Y = -oldVelocity.Y * 0.4f;
+                        }
+                        Projectile.ai[1] += 1f;
+                    }
+                return false;
+            }
             return false;
         }
-
         public override void OnKill(int timeLeft)
         {
             Collision.HitTiles(Projectile.position + Projectile.velocity, Projectile.velocity, Projectile.width, Projectile.height);
