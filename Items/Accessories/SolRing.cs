1e2217008ac88614fe31e32551de341ee7f72bc8
Megaslayer5656 <justinbex08@gmail.com>
2026-02-26 20:45:16 -0500

    Sol Ring and started Blade of Selves

---
+++ Items/Accessories/SolRing.cs
@@ -0,0 +1,35 @@
+﻿using MogMod.Items.Other;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace MogMod.Items.Accessories
+{
+    public class SolRing : ModItem
+    {
+        public new string LocalizationCategory => "Items.Accessories";
+        public override void SetDefaults()
+        {
+            Item.accessory = true;
+            Item.rare = ItemRarityID.Yellow;
+        }
+        public override void UpdateAccessory(Player player, bool hideVisual)
+        {
+            player.statManaMax2 += 40;
+        }
+
+        public override void AddRecipes()
+        {
+            CreateRecipe().
+            AddIngredient(ItemID.GoldBar, 6).
+            AddIngredient(ItemID.FallenStar, 6).
+            AddTile(TileID.Anvils).
+            Register();
+        }
+    }
+}
